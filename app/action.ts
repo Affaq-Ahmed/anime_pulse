8390efbb0431d2b29a8fa9a15d16190ae23055d2
Affaq-Ahmed <aaawan165@gmail.com>
2024-03-29 12:46:18 +0500

    Add fetchAnime function to fetch anime data from API

---
+++ app/action.ts
@@ -0,0 +1,15 @@
+'use server';
+
+export const fetchAnime = async (page: number) => {
+	const response = await fetch(
+		`https://shikimori.one/api/animes?page=${page}&limit=10&order=popularity`
+	);
+
+	if (!response.ok) {
+		throw new Error('Failed to fetch anime');
+	}
+
+	const data = await response.json();
+
+	return data;
+};
