f036ecc0857f02d044a710a18cb05e7c5b190e09
Affaq-Ahmed <aaawan165@gmail.com>
2024-03-29 13:26:19 +0500

    Refactor AnimeCard component and LoadMore component

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { MotionDiv } from './MotionDiv';
 
 export interface AnimeProp {
 	id: string;
@@ -17,9 +18,25 @@ interface Prop {
 	index: number;
 }
 
-function AnimeCard({ anime }: Prop) {
+const variants = {
+	hidden: { opacity: 0 },
+	visible: { opacity: 1 },
+};
+
+function AnimeCard({ anime, index }: Prop) {
 	return (
-		<div className='max-w-sm rounded relative w-full'>
+		<MotionDiv
+			variants={variants}
+			initial='hidden'
+			animate='visible'
+			transition={{
+				duration: index * 0.125,
+				delay: 1,
+				ease: 'easeInOut',
+			}}
+			viewport={{ amount: 0 }}
+			className='max-w-sm rounded relative w-full'
+		>
 			<div className='relative w-full h-[37vh]'>
 				<Image
 					src={`https://shikimori.one${anime.image.original}`}
@@ -67,7 +84,7 @@ function AnimeCard({ anime }: Prop) {
 					</div>
 				</div>
 			</div>
-		</div>
+		</MotionDiv>
 	);
 }
 
